7c4ba8c980edaef81a2cdaf34b0b9e7bb3c34607
jihaneh <jijih@hotmail.fr>
2022-01-03 10:12:20 +0100

    Add display books function

--- script.js
+++ script.js
@@ -4,17 +4,40 @@ const BOOK_COLLECTION = document.querySelector('#book_collection');
 const FORM = document.querySelector('form');
 const ADD_BUTTON = document.querySelector('#add_button');
 
-function addBook () {
+function createHtml(parent, tag) {
+  const element = document.createElement(tag);
+  parent.appendChild(element);
+  return element;
+}
+
+function displayBook() {
+  BOOK_COLLECTION.innerText = '';
+
+  BOOKS.forEach((book) => {
+    const div = createHtml(BOOK_COLLECTION, 'div');
+    const p = createHtml(div, 'p');
+
+    createHtml(p, 'span').innerText = book.title;
+    createHtml(p, 'br');
+    createHtml(p, 'span').innerText = book.author;
+    createHtml(div, 'button').innerText = 'Remove';
+    createHtml(div, 'hr');
+  });
+}
+displayBook();
+
+function addBook() {
   const book = {
     title: FORM.title.value,
     author: FORM.author.value,
-  }
+  };
   BOOKS.push(book);
+  displayBook();
 }
 
 ADD_BUTTON.addEventListener('click', (event) => {
   event.preventDefault();
   addBook();
-  FORM.title.value = "";
-  FORM.author.value = "";
-})
+  FORM.title.value = '';
+  FORM.author.value = '';
+});
